abd9bbab0843ff7d9d005dd8f46b281c40662431
Fran Josipovic <fran.josipovic@atos.net>
2023-09-15 15:58:09 +0200

    initial

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { PagesRoutingModule } from './pages-routing.module';
+
+@NgModule({
+  declarations: [
+    PagesComponent
+  ],
+  imports: [
+    PagesRoutingModule
+  ],
+  providers: [],
+})
+export class PagesModule { }
